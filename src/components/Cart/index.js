af814f3b85dedce4b072d4dc478be5af43b4059a
Tveerababu <babuv9985@gamil.com>
2023-05-23 08:27:09 +0000

    nxt authentication 2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-container">
+      <img
+        alt="cart"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        className="cart-image"
+      />
+    </div>
+  </>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-container">
+      <img
+        alt="products"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        className="product-image"
+      />
+    </div>
+  </>
+)
+export default Products
